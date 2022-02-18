e029fa5a0d8f502559369124b583cf25b7bcd02d
Kaminee Balkawade <kamineedbalkawade@gmail.com>
2022-02-18 22:14:24 +0530

    first seeker to zuul server connct

---
+++ src/main/java/com/kim/balk/gatewayfirstseeker/GatewayFirstseekerApplication.java
@@ -0,0 +1,23 @@
+package com.kim.balk.gatewayfirstseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class GatewayFirstseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayFirstseekerApplication.class, args);
+	}
+	
+	@GetMapping
+	public String welcome(){
+		return "Welcome to Kaminis first gateway seeker page";
+	}
+
+}
---
+++ src/test/java/com/kim/balk/gatewayfirstseeker/GatewayFirstseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.balk.gatewayfirstseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayFirstseekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
